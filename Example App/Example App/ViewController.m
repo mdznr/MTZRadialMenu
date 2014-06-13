f629e8de510b30601d75dda23b0ef8307200351d
Matt Zanchelli <matt@mdznr.com>
2014-06-13 13:46:17 -0400

    Using standard action types in radial menu example.

--- Example App/Example App/ViewController.m
+++ Example App/Example App/ViewController.m
@@ -20,26 +20,20 @@
 	CGRect right = CGRectMake(278, 276, 30, 30);
 	CGRect left = CGRectMake(12, 276, 30, 30);
 	
-	MTZRadialMenu *radialMenu = [[MTZRadialMenu alloc] initWithFrame:middle];
+	MTZRadialMenu *radialMenu = [[MTZRadialMenu alloc] initWithFrame:right];
 	[radialMenu setImage:[UIImage imageNamed:@"Circle"] forState:UIControlStateNormal];
 	[radialMenu setImage:[UIImage imageNamed:@"CircleHighlighted"] forState:UIControlStateSelected];
 	[self.view addSubview:radialMenu];
 	
-	MTZAction *action = [MTZAction actionWithImage:[UIImage imageNamed:@"Circle"]
-								  highlightedImage:[UIImage imageNamed:@"CircleHighlighted"]
-										   handler:^(MTZAction *action) {
-											   NSLog(@"My custom action was handled!");
-										   }];
-	[radialMenu setAction:action forLocation:MTZRadialMenuLocationTop];
+	MTZAction *send = [MTZAction actionOfType:MTZActionTypeConfirm handler:^(MTZAction *action) {
+		NSLog(@"Send");
+	}];
+	[radialMenu setAction:send forLocation:MTZRadialMenuLocationTop];
 	
-	MTZAction *secondAction = [MTZAction actionWithImage:[UIImage imageNamed:@"Circle"]
-										highlightedImage:[UIImage imageNamed:@"CircleHighlighted"]
-												 handler:^(MTZAction *action) {
-													 NSLog(@"My second custom action was handled!");
-												 }];
-	[radialMenu setAction:secondAction forLocation:MTZRadialMenuLocationLeft];
-//	[radialMenu setAction:secondAction forLocation:MTZRadialMenuLocationRight];
-//	[radialMenu setAction:secondAction forLocation:MTZRadialMenuLocationBottom];
+	MTZAction *cancel = [MTZAction actionOfType:MTZActionTypeCancel handler:^(MTZAction *action) {
+		NSLog(@"Cancel");
+	}];
+	[radialMenu setAction:cancel forLocation:MTZRadialMenuLocationLeft];
 }
 
 @end
